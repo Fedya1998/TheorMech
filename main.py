03c7d6f12f1bd2c5d036151ce50f5b319fde394e
Fedya <fedor.chuprakov@mail.ru>
2017-12-14 21:52:14 +0300

    First commit

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import sign_graph
 # 2.27477783 without atmosphere
 # 2.276 with atmosphere
 
-r_min = sign_graph.test(speed=15)
-# sign_graph.show(2)
+# r_min = sign_graph.test(2.3)
+sign_graph.show(2.3)
